5ece1f8ae93a32e205b33128cec2437d81e5f0d1
Dan M. Larsen <larsendanmarius@gmail.com>
2024-08-12 16:22:36 +0200

    Minor styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-const faqListEl = document.querySelector('.faq__list');
+const faqListEl = document.querySelector(".faq__list");
 
-faqListEl.addEventListener('click', e => {
-    const btn = e.target.closest('.faq__question-toggle-btn');
+faqListEl.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if (!btn) return;
+  console.log(e.target);
 
-    const listItemEl = btn.closest('.faq__list-item');
-    listItemEl.classList.toggle('faq__list-item--collapse');
+  const btn =
+    e.target.closest(".faq__question") ||
+    e.target.closest(".faq__question-toggle-btn");
+
+  if (!btn) return;
+
+  const listItemEl = btn.closest(".faq__list-item");
+  listItemEl.classList.toggle("faq__list-item--collapse");
 });
